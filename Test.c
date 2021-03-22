9ba31d5ab340c60d7633e89b93a274bb3c6b72ca
dlopez128 <80988874+dlopez128@users.noreply.github.com>
2021-03-21 23:00:24 -0700

    #2. Made changes to my code by excluding while loop for now. I only used a switch Statement.

--- Test.c
+++ Test.c
@@ -5,41 +5,32 @@ int main()
 	// Variables and initialization 
 	int a, b;
 	char operator;
-	int finished = 0;
 
 	// User types in expression. 
 	printf("\nEnter two operands: \n");
 	scanf_s("%d %d", &a, &b);
+	printf("\nEnter an operator: \n");
+	scanf_s("%c", &operator);
 
 	// Print Results
 	switch (operator) {
+	
 	case '+':
-		printf("%d\n", a + b);
+		printf("%d + %d = %d", a, b, a + b);
 		break;
 
 	case '-':
-		printf("%d\n", a - b);
+		printf("%d - %d = %d", a, b, a - b);
 		break;
 
 	case '*':
-		printf("%d\n", a * b);
+		printf("%d * %d = %d", a, b, a * b);
 		break;
 
 	case '/':
-		printf("%d\n", a / b);
+		printf("%d / %d = %d", a, b, a / b);
 		break;
 
-	}
-
-	while (!finished)
-	{
-		printf("\nEnter an operator: \n");
-		scanf_s("%c", &operator);
-		getchar();
-		printf("\nEnter two operands: \n");
-		scanf_s("%d %d", &a, &b);
-
-		getchar();
 	}
 
 	return 0;
