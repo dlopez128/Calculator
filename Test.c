b523cb4d935906b3ccfe0ee4f84030274b278249
dlopez128 <80988874+dlopez128@users.noreply.github.com>
2021-03-27 01:02:21 -0700

    Application Release 2; Users can enter values for variables a and b.

--- Test.c
+++ Test.c
@@ -1,50 +1,68 @@
 // This code includes a Do While Loop and decision. 
-// My first attempt. 
-// Didn't include function. 
-// Add division error. 
+// My SECOND attempt. 
+// Included function. 
+// Didn't add division error. 
 
 #include <stdio.h>
+#include <conio.h>
 
 int main()
 {
 	// Variables and initialization 
-	int a = 5, b = 2, result;
-	char operator;
+	int i, a, b, choice, result; 
+	void menu(int a, int b, int choice);
+	printf("Enter Two Numbers: \n");
+	printf("a = ");
+	scanf_s("%d", &a);
+	printf("b = ");
+	scanf_s("%d", &b);
 
-	// Do While Loop
-	do {
-		printf("\nEnter an operator: \n");
-		printf("Exit\n");
-
-		scanf_s("%c", &operator);
-		fflush(stdin);
-
-		switch (operator) {
-		case '+':
-			printf("result = %d\n", a + b);
-			break;
-
-		case '-':
-			printf("result = %d\n", a - b);
-			break;
-
-		case '*':
-			printf("result = %d\n", a * b);
-			break;
-
-		case '/':
-			printf("result = %d\n", a / b);
-			break;
-
-		case '5':
-			printf("Thanks\n");
-			break; 
-
-		}
+	// While Loop
+	i = 0; 
+	while (choice != 5)
+	{
+		menu(a, b, choice);
+		i++; 
 	}
 
-	while (operator != '5');
-
-	return 0; 
+	getchar();
+	return 0;
 	
+}
+
+void menu(int a, int b, int choice)
+{
+	int result, i;
+
+	// Infinite Loop 
+	for (i = 0; i < 10; ++i)
+	{
+		printf("1. Addition\n");
+		printf("2. Subtraction\n");
+		printf("3. Multiplication\n");
+		printf("4. Division\n");
+		printf("\n\n");
+		printf("Enter the Number for the Operator: \n");
+		scanf_s("%d", &choice);
+	}
+
+	// Switch Operator
+	switch (choice) 
+	{
+	case '+':
+		printf("result = %d\n", a + b);
+		break;
+	case '-':
+		printf("result = %d\n", a - b);
+		break; 
+	case '*':
+		printf("result = %d\n", a * b);
+		break;
+	case '/':
+		printf("result = %d\n", a / b);
+		break; 
+	default:
+		exit(0);
+
+	}
 }
